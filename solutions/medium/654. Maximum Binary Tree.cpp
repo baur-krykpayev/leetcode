98b4e94d890c034aa9426ab5c0166b2cedc805f4
Baur Krykpayev <bkrykpayev@lutron.com>
2017-11-18 11:48:45 -0500

    654 added

---
+++ solutions/medium/654. Maximum Binary Tree.cpp
@@ -0,0 +1,54 @@
+/* 
+ * Problem: 654. Maximum Binary Tree[medium]
+ * Source : https://leetcode.com/problems/find-right-interval/description/
+ * Solver : Baur Krykpayev
+ * Date   : 11/18/2017
+ */
+ 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) 
+    {
+        return findMaxIndex(nums, 0, nums.size());
+    }
+    
+private:
+    
+    TreeNode* findMaxIndex(vector<int>& nums, int start, int end)
+    {
+        if (start == end)
+        {
+            return NULL;
+        }
+        int ind = maxIndex(nums, start, end);
+        TreeNode* root = new TreeNode(nums[ind]);
+        
+        root->left = findMaxIndex(nums, start, ind);
+        root->right = findMaxIndex(nums, ind + 1, end);
+        
+        return root;
+    }
+    
+    int maxIndex(vector<int>& nums, int start, int end)
+    {
+        int maxInd = start;
+        
+        for (int index = start; index < end; index++)
+        {
+            if (nums[maxInd] < nums[index])
+            {
+                maxInd = index;
+            }    
+        }
+        return maxInd;
+    }
+};
