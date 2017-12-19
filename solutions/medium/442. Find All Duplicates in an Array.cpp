dfa14ba42487278271e317779301633a41119fe6
baur-krykpayev <baur.krykpayev@gmail.com>
2017-12-18 20:13:48 -0500

    442 added

---
+++ solutions/medium/442. Find All Duplicates in an Array.cpp
@@ -0,0 +1,30 @@
+/* 
+ * Problem: 442. Find All Duplicates in an Arra[medium]
+ * Source : https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+ * Solver : Baur Krykpayev
+ * Date   : 12/18/2017
+ */
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) 
+    {
+        vector<int> ret;
+        if (!nums.size()) {return ret;}
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int pos = abs(nums[i]) - 1;
+            if (nums[pos] > 0)
+            {
+                nums[pos] -= 2*nums[pos];
+            }
+            else
+            {
+                ret.push_back(abs(nums[i]));
+            }
+        }
+        
+        return ret;
+    }
+};
